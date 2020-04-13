3e56d3094e0daadf54199dbb3d280d011531c5c0
baruk project <barukproject@gmail.com>
2020-04-13 17:59:30 +0700

    update

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $.getJSON( "https://api.tvmaze.com/search/shows?q=batman", function( data ) {
     let film = data;
-
+    console.log(data);
     $.each(film, function(i, data){
         let genre = '', html = '';
         let len_genre = data.show.genres.length;
@@ -17,23 +17,21 @@ $.getJSON( "https://api.tvmaze.com/search/shows?q=batman", function( data ) {
                 <button type="button" class="btn btn-success" data-toggle="modal" data-target="#exampleModalCenter-${i}">See Image</button>
                 <div class="modal fade" id="exampleModalCenter-${i}" >
                     <div class="modal-dialog modal-dialog-centered" role="document">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title text-dark text-center" id="exampleModalLongTitle">${data.show.name}<h5>
-                                <buttontype="button"class="close"data-dismiss="modal"aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
+                        <div class="modal-content bg-dark">
+                            <div class="modal-header  d-flex justify-content-center ">
+                                <h3 class="modal-title text-white text-center" id="exampleModalLongTitle">${data.show.name}<h3>
                             </div>
-                        <div class="modal-body">
-                            <img src="${data.show.image.medium}"/>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="text-center btn btn-secondary" data-dismiss="modal">Close</button>
+                            <div class="modal-body">
+                                <img src="${data.show.image.medium}"/>
+                            </div>
+                            <div class="modal-footer d-flex flex-column">
+                                <p class="mb-3 mt-2">${data.show.summary}</p>
+                                <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </td>
-        </tr>`;
+                </td>
+            </tr>`;
         $('#daftar-film').append(html);
     }); 
    });
